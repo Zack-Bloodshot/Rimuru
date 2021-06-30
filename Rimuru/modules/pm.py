8b8d6a77bd0b67991fd7d09754a01247f965cc7d
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-30 18:44:38 +0530

    update pm.py

--- Rimuru/modules/pm.py
+++ Rimuru/modules/pm.py
@@ -14,9 +14,9 @@ async def pmperm(slime):
   user = await slime.get_chat()
   if user.bot:
     return
-  name = f"[{user.first_name}](tg://user?id={user.id})"
+  name = mention(user.first_name, user.id)
   if not slime.media:
-    await rafael.send_message(pm_log, f"**{name}**: {slime.message.text}", parse_mode='md')
+    await rafael.send_message(pm_log, f"**{name}**: {slime.message.text}")
   else:
     await rafael.send_file(pm_log, file=slime.media, caption=f"{name}: {slime.message.text}")
   if pm.is_approved(user.id):
