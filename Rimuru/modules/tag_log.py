048c50a0bd8575252e60e472ae051fe8b2172af9
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-30 17:08:53 +0530

    update tag_log.py

--- Rimuru/modules/tag_log.py
+++ Rimuru/modules/tag_log.py
@@ -7,7 +7,7 @@ async def taglog(slime):
   user = slime.sender 
   if user.bot:
     return 
-  kek = mention(user.first_name, user.id)
+  kek = f"[{user.first_name}](tg://user?id={})"
   chat = slime.chat
   msg = slime.message
   if msg.text:
