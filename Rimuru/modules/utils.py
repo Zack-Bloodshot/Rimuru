ccfa74dfb39178df369465bb61356ae26cda0394
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-30 11:55:40 +0530

    Add files via upload

---
+++ Rimuru/modules/utils.py
@@ -0,0 +1,28 @@
+from Rimuru import rimuru 
+from telethon import events, Buttons
+import time 
+
+@rimuru.on(events.NewMessage(outgoing=True,pattern=r'#^purge(.*)'))
+async def purge(slime):
+  msg = slime.message.split(" ", 3)
+  reply = await slime.get_reply()
+  if not reply:
+    slime.edit("Reply...")
+    time.sleep(1)
+    slime.delete()
+  from_msg = reply.id
+  try:
+    to_msg = msg[2]
+  except IndexError:
+    to_msg = False 
+  count = 0
+  async for msg in rimuru.iter_messages(slime.chat_id, offset_id=int(from_msg)):
+    if not to_msg == False:
+      if count == int(to_msg):
+        break 
+    try:
+      await msg.delete()
+    except Exception:
+      pass
+  if not count == 0:
+    slime.respond(f"Purged {count} messages..")
