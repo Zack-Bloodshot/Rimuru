19765637c446e33ccd75299a3fbb384fe49883f1
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-30 15:33:17 +0530

    update __main__.py

---
+++ Rimuru/modules/Rimuru/__main__.py
@@ -0,0 +1,12 @@
+from Rimuru import rimuru 
+import logging  
+import Rimuru.modules 
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
+
+logger = logging.getLogger("__name__")
+
+print("Starting....")
+rimuru.parse_mode = 'md'
+rimuru.start()
+rimuru.run_until_disconnected()
