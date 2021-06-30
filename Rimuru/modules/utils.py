accdf55be157955ee71dce3cba33c6239bce713f
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-30 20:26:23 +0530

    update utils.py

--- Rimuru/modules/utils.py
+++ Rimuru/modules/utils.py
@@ -7,10 +7,10 @@ async def purge(slime):
   msg = slime.message.text.split(" ", 3)
   reply = await slime.get_reply_message()
   if not reply:
-    slime.edit("Reply...")
+    await slime.edit("Reply...")
     time.sleep(1)
-    slime.delete()
-  from_msg = reply.id
+    await slime.delete()
+  from_msg = reply.id - 1
   try:
     to_msg = msg[2]
   except IndexError:
@@ -27,3 +27,20 @@ async def purge(slime):
       pass
   if not count == 0:
     await slime.respond(f"Purged {count} messages..")
+    
+@rimuru.on(event.NewMessage(outgoing=True, pattern=r'^#id'))    
+async def idscrape(slime):
+    if slime.reply_to_msg_id:
+        await slime.get_input_chat()
+        r_msg = await slime.get_reply_message()
+        if r_msg.media:
+            bot_api_file_id = pack_bot_file_id(r_msg.media)
+            await slime.edit("**Current Chat ID:**  `{}`\n**From User ID:**  `{}`\n**Bot API File ID:**  `{}`".format(str(event.chat_id),str(r_msg.sender_id),bot_api_file_id))
+        else:
+            await slime.edit("**Chat ID:**  `{}`\n**User ID:**  `{}`".format(str(event.chat_id),str(r_msg.sender_id)))
+    elif event.pattern_match.group(1):
+        ids = await get_user_id(event.pattern_match.group(1))
+        await slime.edit("**Chat ID:**  `{}`\n**User ID:**  `{}`".format(str(event.chat_id),str(ids)))
+    else:
+        await slime.edit("**Current Chat ID:**  `{}`".format(str(event.chat_id)))
+    
