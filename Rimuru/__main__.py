2c56b8cb7a0069805b64182179e6c3c79e02374a
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-30 11:51:07 +0530

    Add files via upload

---
+++ Rimuru/__main__.py
@@ -0,0 +1,10 @@
+from Rimuru import rimuru 
+import logging  
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
+
+logger = logging.getLogger("__name__")
+
+print("Starting....")
+rimuru.start()
+rimuru.run_until_disconnected()
---
+++ Rimuru/_u.py
@@ -0,0 +1,17 @@
+from Rimuru import rimuru, rafael, mention
+from telethon import events, Buttons
+import time
+
+@rimuru.on(events.NewMessage(outgoing=True, pattern=r'^#(alive)'))
+async def alive(slime):
+  me = await rimuru.get_me()
+  kek = mention(me.first_name, me.id)
+  slime.edit(f"*「Yos, Great Sage on!」*\n\nOwner: *{kek}*")
+  
+@rimuru.on(events.NewMessage(outgoing=True, pattern=r'^#ping'))
+async def ping(slime):
+  start = time.time()
+  slime.edit("`۞pinging....`")
+  end = time.time()
+  pon = (start - end).microseconds / 100
+  slime.edit(f"ᑭOᑎᘜ!!\n\npong time: {pon}ms")
