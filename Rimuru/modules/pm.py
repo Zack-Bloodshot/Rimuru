2a75689f2f5d9fbaf1cbb55999c59ff2865d7548
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-30 16:18:20 +0530

    update pm.py

--- Rimuru/modules/pm.py
+++ Rimuru/modules/pm.py
@@ -12,7 +12,7 @@ async def pmperm(slime):
   if not slime.is_private:
     return
   user = await slime.get_chat()
-  name = f"[{user.first_name}](tg://user?id={user.id})"
+  name = mention(user.first_name, user.id)
   if not slime.media:
     await rafael.send_message(pm_log, f"**{name}**: {slime.message.text}", parse_mode='md')
   if pm.is_approved(user.id):
