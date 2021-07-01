af4f6ea3f206d740f54a9b6c139dbd8a9cbaf06a
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-07-01 11:30:44 +0530

    Add files via upload

---
+++ Rimuru/modules/songs.py
@@ -0,0 +1,14 @@
+from telethon import events, Buttons 
+from Rimuru import rimuru 
+from pytube import *
+import os
+
+@rimuru.on(events.NewMessage(outgoing=True,pattern=r'^!s(.*)'))
+async def songs(slime):
+  args = slime.message.text[2:]
+  await slime.delete()
+  yt = Youtube(args)
+  yt = yt.streams.get_only_audio(mp3)
+  dl = yt.download()
+  await rimuru.send_file(slime.chat_id, file=dl)
+  os.remove(dl)
