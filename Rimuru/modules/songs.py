39ceb31a7749f54f46bc9cc7e6e77c848fefdda7
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-07-02 16:05:34 +0530

    update songs.py

--- Rimuru/modules/songs.py
+++ Rimuru/modules/songs.py
@@ -1,11 +1,33 @@
 from telethon import events, Button 
 from Rimuru import rimuru 
-from pytube import *
+import youtube_dl
+import logging
 import os
 from youtube_search import YoutubeSearch
 
+LOGS = logging.getLogger(__name__)
+
 @rimuru.on(events.NewMessage(outgoing=True,pattern=r'^!s(.*)'))
 async def songs(slime):
+  opts = {
+    "outtmpl": "%(title)s.%(ext)s",
+    "logger": LOGS,
+    "writethumbnail": True,
+    "prefer_ffmpeg": True,
+    "format": "bestaudio/best",
+    "geo_bypass": True,
+    "nocheckcertificate": True,
+    "postprocessors": [
+      {
+        "key": "FFmpegExtractAudio",
+        "preferredcodec": "mp3",
+        "preferredquality": uid,
+      },
+      {"key": "EmbedThumbnail"},  # ERROR: Conversion failed!
+      {"key": "FFmpegMetadata"},
+    ],
+    "quiet": True,
+  }
   args = slime.message.text[2:]
   if args.startswith("https://"):
     url = args
@@ -13,12 +35,10 @@ async def songs(slime):
     result = YoutubeSearch(args,max_results=1).to_dict()
     url = "https://youtu.be/" + result[0]['id']
   await slime.delete()
-  yt = YouTube(url)
-  ytu = yt.streams.get_audio_only()
-  dl = ytu.download()
-  path, ext = os.path.splitext(dl)
-  file_name = path + ".mp3"
-  dl = os.rename(dl, file_name)
+  with youtube_dl.YouTubeDL(opts) as ydl:
+    dl = ydl.download(url)
+  m = await slime.respond("Downloaded, Now uploading....")
   async with rimuru.action(slime.chat_id, 'audio'):
-    await rimuru.send_message(slime.chat_id, file=open(file_name, 'rb'), force_document=False)
+    await rimuru.send_message(slime.chat_id, file=open(dl, 'rb'), force_document=False)
+  await m.delete()
   os.remove(file_name)
