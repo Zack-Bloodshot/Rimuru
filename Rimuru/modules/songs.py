2e80e81e9b74f0f0ca0a8377076f276718df50e6
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-07-02 13:08:11 +0530

    update songs.py

--- Rimuru/modules/songs.py
+++ Rimuru/modules/songs.py
@@ -2,16 +2,23 @@ from telethon import events, Button
 from Rimuru import rimuru 
 from pytube import *
 import os
+from youtube_search import YoutubeSearch
 
 @rimuru.on(events.NewMessage(outgoing=True,pattern=r'^!s(.*)'))
 async def songs(slime):
   args = slime.message.text[2:]
+  if args.startswith("https://"):
+    url = args
+  else:
+    result = YoutubeSearch(query=args,max_results=1).to_dict()
+    url = "https://youtu.be/" + results[0]['id']
   await slime.delete()
-  yt = YouTube(args)
+  yt = YouTube(url)
   ytu = yt.streams.get_audio_only()
   dl = ytu.download()
   path, ext = os.path.splitext(dl)
   file_name = path + ".mp3"
   dl = os.rename(dl, file_name)
-  await rimuru.send_file(slime.chat_id, file=open(file_name, 'rb'))
+  async with rimuru.action(slime.chat_id, 'audio'):
+    await rimuru.send_file(slime.chat_id, file=open(file_name, 'rb'), force_document=False)
   os.remove(file_name)
