80a26ed86d66f1207c2939180792995cb9d9a633
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-07-29 12:26:18 +0530

    button maker v0

--- Rimuru/modules/utils.py
+++ Rimuru/modules/utils.py
@@ -3,6 +3,14 @@ from telethon import events, Button
 from telethon.utils import pack_bot_file_id
 import time 
 
+helper = '''
+
+`#purge`: reply to message to delete every message till the latest message 
+`#id`: reply to anything to get the respective ids 
+
+
+'''
+
 @rimuru.on(events.NewMessage(outgoing=True,pattern=r'^#purge(.*)'))
 async def purge(slime):
   msg = slime.message.text.split(" ", 3)
@@ -44,4 +52,36 @@ async def idscrape(slime):
         await slime.edit("**Chat ID:**  `{}`\n**User ID:**  `{}`".format(str(slime.chat_id),str(ids)))
     else:
         await slime.edit("**Current Chat ID:**  `{}`".format(str(slime.chat_id)))
-    
+
+@rafael.on(events.InlineQuery(pattern=r'^btn(.*)')) 
+async def btn_maker(slime):
+  try:
+    the_btn = slime.split(' ', 1)[1]
+  except IndexError:
+    await slime.answer(
+      [
+        slime.builder.article(
+          title='Build Button',
+          description='Make buttons Like, Text To send, Button1:url Button2:url',
+          text= 'Make buttons Like, Text To send, Button1:url Button2:url',
+          )
+        ]
+      )
+  hek = the_btn.split(',')
+  main_text = hek[0]
+  hek.pop(0)
+  btns = []
+  for i in hek:
+    btn = i.split(':', 1)
+    btn_text = btn[0]
+    btn_url = btn[1]
+    btns.append([Button.url(text=btn_text), url=btn_url])
+  article = [
+    slime.builder.article(
+      title=f'{main_text[:-10]}...',
+      description=f'button ready!, Click To send...',
+      text=main_text,
+      buttons=btns,
+      )
+    ]
+   await slime.answer(article)
