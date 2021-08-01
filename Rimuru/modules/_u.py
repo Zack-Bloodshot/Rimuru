0454e45c72dc9c30fcae20b71ab6f8a98c044656
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-08-01 20:10:35 +0530

    -

--- Rimuru/modules/_u.py
+++ Rimuru/modules/_u.py
@@ -18,9 +18,20 @@ async def ping(slime):
   pon = (start - end).microseconds / 1000
   await slime.edit(f"ᑭOᑎᘜ!!\n\npong time: {pon}ms")
 
-@rafael.on(events.NewMessage(outgoing=True,pattern=r'^/start'))
+@rafael.on(events.NewMessage(outgoing=True,pattern=r'^\/start'))
 async def start(slime):
-  await slime.reply("Im up!")
+  if slime.message.text != 'start':
+    query = slime.message.text.split(' ', 1)[1]
+    if query == 'help':
+      await slime.reply(help_strings)
+      return
+    else:
+      pass
+  bot = await rafael.get_me()
+  user = await rimuru.get_me()
+  text = f'Im {bot.first_name} a great sage, and helper for {user.first_name}'
+  await slime.reply(text)
+
   
 @rafael.on(events.InlineQuery(pattern=r'help'))
 async def helppp(slime):
@@ -34,7 +45,7 @@ async def helppp(slime):
     buttons=[
       Button.url(
         text='Help',
-        url=f'https://t.me/{rafael_name},
+        url=f'https://t.me/{rafael_name}?start=help,
         )
       ]
   )
@@ -44,7 +55,8 @@ async def helppp(slime):
 @rafael.on(events.CallbackQuery(pattern=b'help'))
 async def helpcall(slime):
   await slime.answer(help_strings, alert=True)
-  
+
+@rafael.on()  
   
 @rimuru.on(events.NewMessage(outgoing=True,pattern=r'^#help$'))  
 async def helpp(slime):
